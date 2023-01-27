5fd9c0f7e22ec4ac05f045de80e8888df1132357
Vadim Chistiakov <v.chistyakov@prequelapp.com>
2023-01-27 18:30:58 +0100

    change to SwiftUI

---
+++ CriptoAssessment/CriptoAssessment/CriptoAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CriptoAssessmentApp.swift
+//  CriptoAssessment
+//
+//  Created by Vadim Chistiakov on 27.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CriptoAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
